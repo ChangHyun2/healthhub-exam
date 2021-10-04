97545c0f25df689aa48c7645beab0c815499ab6a
ChangHyun2 <jun@localhost.local>
2021-10-04 20:02:57 +0900

    feat: clearMole

--- src/hooks/mole.ts
+++ src/hooks/mole.ts
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const useMole: () => [boolean, () => void] = () => {
-    // TODO
-    const [mole] = useState(true);
-    const clearMole = () => {
-        console.log('Mole hit!');
-    };
+  // TODO
+  const [mole, setMole] = useState(true);
 
-    return [mole, clearMole];
-}
+  const clearMole = () => {
+    console.log("Mole hit!");
+    setMole((prev) => false);
+  };
+
+  useEffect(() => {
+    !mole && setTimeout(() => setMole(true), 1 + 10 * Math.random());
+  }, [mole]);
+
+  return [mole, clearMole];
+};
 export default useMole;
